541510696961e4b62464c25de6507f749b46fcbc
DavidChen617 <a880617c@gmail.com>
2026-05-25 16:01:13 +0800

    feat: add cursor head file

---
+++ src/cursor.h
@@ -0,0 +1,19 @@
+#ifndef CURSOR_H
+#define CURSOR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "table.h"
+
+typedef struct Cursor{
+    Table *table;
+    uint32_t page_num;
+    uint32_t cell_num;
+    bool end_of_table;
+} Cursor;
+
+Cursor *table_start(Table *table);
+void *cursor_value(Cursor *cursor);
+void cursor_advance(Cursor *cursor);
+
+#endif
